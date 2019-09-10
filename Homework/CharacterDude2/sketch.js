f2d661737e8d2405afbf396aeefc6e339b5f1111
Diana Matei <iixdmatei@gmail.com>
2019-09-10 11:48:33 -0400

    added semicolons

--- Homework/CharacterDude2/sketch.js
+++ Homework/CharacterDude2/sketch.js
@@ -24,36 +24,36 @@ background('LemonChiffon');
   pointX = mouseX;
   pointY = mouseY;
 
-  fill('black') //fill for legs
-  strokeWeight(2) //strokeWeight for legs
-  line(pointX-35,pointY+100,pointX-35,pointY+150) //legs
-  line(pointX+35,pointY+100,pointX+35,pointY+150) //legs
+  fill('black'); //fill for legs
+  strokeWeight(2); //strokeWeight for legs
+  line(pointX-35,pointY+100,pointX-35,pointY+150); //legs
+  line(pointX+35,pointY+100,pointX+35,pointY+150); //legs
 
-  line(pointX+35,pointY+50,pointX+100,pointY-50) //arms
-  line(pointX-35,pointY+50,pointX-100,pointY-50) //arms
+  line(pointX+35,pointY+50,pointX+100,pointY-50); //arms
+  line(pointX-35,pointY+50,pointX-100,pointY-50); //arms
 
-  fill('orange') //body of dorito
-  strokeWeight(1)
-  triangle(pointX,pointY-50,pointX-100,pointY+100,pointX+100,pointY+100) //body of dorito
+  fill('orange'); //body of dorito
+  strokeWeight(1);
+  triangle(pointX,pointY-50,pointX-100,pointY+100,pointX+100,pointY+100); //body of dorito
 
-  fill('white') //white of the eye
-  circle(pointX-35,pointY+50,eyeDiameterWhite)
-  circle(pointX+35,pointY+50,eyeDiameterWhite)
+  fill('white'); //white of the eye
+  circle(pointX-35,pointY+50,eyeDiameterWhite);
+  circle(pointX+35,pointY+50,eyeDiameterWhite);
 
-  fill('black') //pupil of the eye
-  circle(pointX-35,pointY+50,eyeDiameterPupil)
-  circle(pointX+35,pointY+50,eyeDiameterPupil)
+  fill('black'); //pupil of the eye
+  circle(pointX-35,pointY+50,eyeDiameterPupil);
+  circle(pointX+35,pointY+50,eyeDiameterPupil);
 
-  rect(pointX-35,pointY+70,70,20) //mouth with black fill
+  rect(pointX-35,pointY+70,70,20); //mouth with black fill
 
-  fill('white')
-  rect(pointX-30,pointY+75,60,10) //teeth
+  fill('white');
+  rect(pointX-30,pointY+75,60,10); //teeth
 
 
-  fill('black')
-  strokeWeight(0)
-  rect(pointX-36,pointY+150,footWidth,footHeight) //little bootie feet
-  rect(pointX+34,pointY+150,footWidth,footHeight)
+  fill('black');
+  strokeWeight(0);
+  rect(pointX-36,pointY+150,footWidth,footHeight); //little bootie feet
+  rect(pointX+34,pointY+150,footWidth,footHeight);
 
 
 
